d40692e5a88f6f7a33d97a088f7dd4e9ea2dfb4d
한태규 <gksxorb147@gmail.com>
2021-12-24 16:34:25 +0900

    2021-12-24

---
+++ src/ch9/CardToString2.java
@@ -0,0 +1,30 @@
+package ch9;
+
+//class Card {
+//    String kind;
+//    int number;
+//
+//    Card() {
+//        this("SPADE", 1);
+//    }
+//
+//    Card(String kind, int number) {
+//        this.kind = kind;
+//        this.number = number;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "kind : " + kind + ", number : " + number;
+//    }
+//}
+
+public class CardToString2 {
+    public static void main(String[] args) {
+//        Card c1 = new Card();
+//        Card c2 = new Card();
+//
+//        System.out.println(c1.toString());
+//        System.out.println(c2.toString());
+    }
+}
