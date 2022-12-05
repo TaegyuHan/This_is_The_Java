b89398c75518a8ea6fcf1c28a18c2fa6465fd60b
한태규 <gksxorb147@gmail.com>
2022-12-05 20:33:10 +0900

    Chapter01 3. 자바로 프로그램 작성하기 3.1 Hello.java

---
+++ src/main/java/chapter01/Hello.java
@@ -0,0 +1,13 @@
+package chapter01;
+
+
+/**
+ * 작성날짜: 2022-12-05
+ * 작성자: 한태규
+ * 내용: Java 프로그램 동작확인
+ * */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, world.");
+    }
+}
