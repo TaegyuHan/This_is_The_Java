e9bf09a1aef154dde955ef18316059fb1bb191dc
한태규 <gksxorb147@gmail.com>
2022-12-19 21:56:40 +0900

    Chapter06 객체지향언어 5. 생성자(Constructor) 5.1 생성자란? 5.2 기본 생성자(default constructor) 5.3 매개변수가 있는 생성자 5.4 생성자에서 다른 생성자 호출하기 - this(), this 5.5 생성자를 이용한 인스턴스의 복사 Page : 291 ~ 299

--- src/main/java/chapter06/CardTest.java
+++ src/main/java/chapter06/CardTest.java
@@ -1,17 +1,28 @@
 package chapter06;
 
 class Car {
-    String color;        // 색상
+    String color;		// 색상
     String gearType;    // 변속기 종류 - auto(자동), manual(수동)
-    int door;            // 문의 개수
+    int door;			// 문의 개수
 
     Car() {
+        this("white", "auto", 4);
     }
 
-    Car(String c, String g, int d) {
-        color = c;
-        gearType = g;
-        door = d;
+    Car(String color) {
+        this(color, "auto", 4);
+    }
+
+    Car(Car c) {	// 인스턴스의 복사를 위한 생성자.
+        color    = c.color;
+        gearType = c.gearType;
+        door     = c.door;
+    }
+
+    Car(String color, String gearType, int door) {
+        this.color    = color;
+        this.gearType = gearType;
+        this.door     = door;
     }
 }
 
