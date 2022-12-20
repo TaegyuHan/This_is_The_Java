bf3c410750df3d9e1238ae482a75687f8209984a
한태규 <gksxorb147@gmail.com>
2022-12-20 11:50:07 +0900

    Chapter07 객체지향언어 2. 오버라이딩(overriding) 2.1 오버라이딩이란? 2.2 오버라이딩의 조건 2.3 오버라이딩 vs 오버라이딩 2.4 super 2.5 super() - 조상 클래스의 생성자 Page : 327 ~ 335

---
+++ src/main/java/chapter07/PointTest.java
@@ -0,0 +1,26 @@
+package chapter07;
+
+class PointTest {
+    public static void main(String args[]) {
+        Point3D p3 = new Point3D(1,2,3);
+    }
+}
+
+class Point3D extends Point {
+    int z;
+
+    Point3D() {
+        this(100, 200, 300);
+    }
+
+    Point3D(int x, int y, int z) {
+        super(x, y);
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    String getLocation() {	// ¿À¹ö¶óÀÌµù
+        return "x :" + x + ", y :"+ y + ", z :" + z;
+    }
+}
