ee192fa89157e27fc666e9892303c2b0c4f3314a
한태규 <gksxorb147@gmail.com>
2022-12-21 15:30:46 +0900

    Chapter08 예외처리 1. 예외처리 (exception handling) 1.1 프로그램 오류 1.2 예외 클래스의 계층 구조 1.3 예외 처리하기 - try - catch 문 1.4 try - catch문에서의 흐름 1.5 예외 발생과 catch 블럭 1.6 예외 발생 시키기 1.7 메서드에 예외 선언하기 1.8 finally 블럭 1.9 자동 자원 반환 try - with - resources문 1.10 사용자정의 예외 만들기 1.11 예외 되던지기 excpetion re - throwing 1.13 연결된 예외(chained exception) Page : 414 ~ 447

---
+++ src/main/java/chapter08/ExceptionEx13.java
@@ -0,0 +1,16 @@
+package chapter08;
+
+public class ExceptionEx13 {
+    public static void main(String[] args) {
+        method1();
+    } // main 메서드의 끝
+
+    static void method1() {
+        try {
+            throw new Exception();
+        } catch (Exception e) {
+            System.out.println("method1메서드에서 예외가 처리되었습니다.");
+            e.printStackTrace();
+        }
+    } // method1의 끝
+}
---
+++ src/main/java/chapter08/ExceptionEx2.java
@@ -0,0 +1,13 @@
+package chapter08;
+
+public class ExceptionEx2 {
+    public static void main(String[] args) {
+        int number = 10;
+        int result = 0;
+
+        for (int i = 0; i < 10; i++) {
+            result = number / (int) (Math.random() * 10);
+            System.out.println(result);
+        }
+    }
+}
